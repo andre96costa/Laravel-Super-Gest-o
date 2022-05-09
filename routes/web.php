6cdb5c7e2ad0070fa615a9a866f8019445d111c2
Andre Costa <andre2396costa@gmail.com>
2022-05-09 14:34:49 -0300

    Nomeando as rotas

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/sobre-nos', 'SobreNosController@sobrenos');
+Route::get('/sobre-nos', 'SobreNosController@sobrenos')->name('site.sobrenos');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
-Route::get('/login', function () {return 'Login';});
+Route::get('/login', function () {return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function () {return 'clientes';});
-    Route::get('/fornecedores', function () {return 'fornecedores';});
-    Route::get('/produtos', function () {return 'produtos';});  
+    Route::get('/clientes', function () {return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function () {return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function () {return 'produtos';})->name('app.produtos');
 });
