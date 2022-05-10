111febb949fce3b5259f7b241d5f1c2c3161e934
Andre Costa <andre2396costa@gmail.com>
2022-05-10 12:03:01 -0300

    Utilizando components

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -2,8 +2,6 @@
 
 @section('title', 'Contato')
 @section('content')
-
-   
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
             <h1>Entre em contato conosco</h1>
@@ -11,24 +9,10 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                <form>
-                    <input type="text" placeholder="Nome" class="borda-preta">
-                    <br>
-                    <input type="text" placeholder="Telefone" class="borda-preta">
-                    <br>
-                    <input type="text" placeholder="E-mail" class="borda-preta">
-                    <br>
-                    <select class="borda-preta">
-                        <option value="">Qual o motivo do contato?</option>
-                        <option value="">Dúvida</option>
-                        <option value="">Elogio</option>
-                        <option value="">Reclamação</option>
-                    </select>
-                    <br>
-                    <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                    <br>
-                    <button type="submit" class="borda-preta">ENVIAR</button>
-                </form>
+                @component('site.layouts.components.form-contato', ['classe' => 'borda-preta'])
+                    <p>Nossa equipe analisará a sua mensagem e retornaremos o mais brevemente possivel</p>
+                    <p>Nosso tempo médio de resposta é 48 horas.</p>
+                @endcomponent
             </div>
         </div>  
     </div>
