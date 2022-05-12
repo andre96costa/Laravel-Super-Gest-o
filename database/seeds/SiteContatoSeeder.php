5059080da2474b262cda9a2226968dbe9fab07b9
Andre Costa <andre2396costa@gmail.com>
2022-05-12 11:21:28 -0300

    Utilizando factory

--- database/seeds/SiteContatoSeeder.php
+++ database/seeds/SiteContatoSeeder.php
@@ -12,12 +12,15 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
-        $contato = new SiteContato();
-        $contato->nome = 'Sistema SG';
-        $contato->telefone = '(11) 99999-9999';
-        $contato->email = 'contato@sg.com.br';
-        $contato->motivo_contato = 1;
-        $contato->mensagem = 'Seja bem-vindo ao sistema Super Gestão';
-        $contato->save();
+        // $contato = new SiteContato();
+        // $contato->nome = 'Sistema SG';
+        // $contato->telefone = '(11) 99999-9999';
+        // $contato->email = 'contato@sg.com.br';
+        // $contato->motivo_contato = 1;
+        // $contato->mensagem = 'Seja bem-vindo ao sistema Super Gestão';
+        // $contato->save();
+
+        //Usando factory 
+        factory(SiteContato::class, 100)->create();
     }
 }
