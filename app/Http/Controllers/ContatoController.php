e3f1e4397d1b2255290f2faf7cd53ba09f074a94
Andre Costa de Miranda Reis <55255924+andre96costa@users.noreply.github.com>
2022-05-12 17:10:48 -0300

    Trabalhando com formularios (#4)
    * Persistindo dados no banco por request
    
    * Validacao do formulario
    
    * Refactor SiteContato e criacao do MotivoContato
    
    * Refactoring, ajustes apos mudanca em SiteContato
    
    * Aplicando validação no form
    
    * ajuste na tela do formulario

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,12 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\MotivoContato;
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
 
-        return view('site.contato');
+        $motivo_contatos = MotivoContato::all();
+        
+        return view('site.contato', compact('motivo_contatos'));
+    }
+
+    public function salvar(Request $request)
+    {
+        // $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+        // $contato->save();
+
+        $request->validate([
+            "nome" => 'required|min:3|max:40',
+            "telefone" => 'required',
+            "email" => 'required|email',
+            "motivo_contatos_id" => 'required',
+            "mensagem" => 'required|max:2000',
+        ],
+        [
+            'nome.min' => 'O campo nome precisa ter no minimo 3 caracteres.',
+            'nome.max' => 'O campo nome precisa ter no máxomo 30 caracteres.',
+            'email.email' => 'O campo email não está no formato correto.',
+            'motivo_contatos_id.required' => 'O campo motivo precisa ser preenchido.',
+            'mensagem.max' => 'A mensagem excedeu a quantidade permitida de caracteres.',
+            'required' => 'O campo :attribute precisa ser preenchido'
+        ]);
+       
+        SiteContato::create([
+            'nome' => $request->input('nome'),
+            'telefone' => $request->input('telefone'),
+            'email' => $request->input('email'),
+            'motivo_contatos_id' => $request->input('motivo_contatos_id'),
+            'mensagem' => $request->input('mensagem'),
+        ]);
+
+        return redirect()->route('site.index');
     }
 }
--- resources/views/site/layouts/components/form-contato.blade.php
+++ resources/views/site/layouts/components/form-contato.blade.php
@@ -3,20 +3,35 @@
 <form action="{{ route('site.contato') }}" method="POST">
     @method('POST')
     @csrf
-    <input type="text" placeholder="Nome" class="{{ $classe }}" name="nome">
+    <input type="text" placeholder="Nome" class="{{ $classe }}" name="nome" value="{{ old('nome') }}">
     <br>
-    <input type="text" placeholder="Telefone" class="{{ $classe }}" name="telefone">
+    @error('nome')
+       <span style="color: red;"> {{ $message }}</span>
+    @enderror
+    <input type="text" placeholder="Telefone" class="{{ $classe }}" name="telefone" value="{{ old('telefone') }}">
     <br>
-    <input type="text" placeholder="E-mail" class="{{ $classe }}" name="email">
+    @error('telefone')
+       <span style="color: red;"> {{ $message }}</span>
+    @enderror
+    <input type="text" placeholder="E-mail" class="{{ $classe }}" name="email" value="{{ old('email') }}">
     <br>
-    <select class="{{ $classe }}" name="motivo_contato">
+    @error('email')
+       <span style="color: red;"> {{ $message }}</span>
+    @enderror
+    <select class="{{ $classe }}" name="motivo_contatos_id">
         <option value="">Qual o motivo do contato?</option>
-        <option value="1">Dúvida</option>
-        <option value="2">Elogio</option>
-        <option value="3">Reclamação</option>
+        @foreach ($motivo_contatos as $key => $value)
+            <option value="{{ $value->id }}" {{ old('motivo_contatos_id') == $value->id ? 'selected' : ''}}>{{ $value->motivo_contato }}</option>
+        @endforeach
     </select>
     <br>
-    <textarea class="{{ $classe }}" name="mensagem">Preencha aqui a sua mensagem</textarea>
+    @error('motivo_contatos_id')
+       <span style="color: red;"> {{ $message }}</span>
+    @enderror
+    <textarea class="{{ $classe }}" name="mensagem">{{ old('mensagem') ?? 'Preencha aqui a sua mensagem'}}</textarea>
     <br>
+    @error('mensagem')
+       <span style="color: red;"> {{ $message }}</span>
+    @enderror
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
