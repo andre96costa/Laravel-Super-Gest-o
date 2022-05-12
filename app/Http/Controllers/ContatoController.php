6fe66aafaf8b45922bea139e26997fecc1f4df4c
Andre Costa <andre2396costa@gmail.com>
2022-05-12 11:48:15 -0300

    Persistindo dados no banco por request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
+
+        // $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+        // $contato->save();
+
+        SiteContato::create([
+            'nome' => $request->input('nome'),
+            'telefone' => $request->input('telefone'),
+            'email' => $request->input('email'),
+            'motivo_contato' => $request->input('motivo_contato'),
+            'mensagem' => $request->input('mensagem'),
+        ]);
 
         return view('site.contato');
     }
